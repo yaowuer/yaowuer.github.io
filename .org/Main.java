8f20e10f34acc941b9e8112b6bce2a4ee21244e5
yaowuer.teacher <yaowuer@qq.com>
2019-06-11 15:01:34 +0800

    .org

---
+++ .org/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main (String... args) {
+
+    }
+
+    public String getCmdPath (String dir, String cmd) {
+        if (dir.contains(":")) { // 绝对路径
+            return getPathFile(cmd);
+        } else { // 相对路径
+            String fname = dir + "/" + cmd;
+            File f = getPathFile(fname);
+            if (f != null) { // 是否在当前文件夹下存在对应的文件
+                return f.getAbsolutePath();
+            } else { // 如果当前目录下没有，那么迭代环境变量查找
+                for (String p : System.getenv("path").split(";")) {
+                    File f = getPathFile(p + "/" + cmd);
+                    if (f.exists()) return f.getAbsolutePath();
+                }
+                return null;
+            }
+        }
+    }
+    private String getPathFile (String path) {
+        if (path.contains(".") && new File(path).exists()) { // 如果路径具备后缀名，并且存在这个文件
+            return path;
+        }
+        if (!path.contains(".")) {
+            for (String p : Arrays.asAlist(".cmd", ".exe", ".bat")) {
+                File f = new File (path + "/" + p);
+                if (f.exists()) return f.getAbsolutePath();
+            }
+        }
+        return null;
+    }
+}
